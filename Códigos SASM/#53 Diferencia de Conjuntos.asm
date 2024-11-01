8ef866de4fadb5ada6ca1d4d123a7d48bf8b0285
yesi07111 <yeseniavr07111@gmail.com>
2024-11-01 16:45:38 -0400

    update SASM code

--- Códigos SASM/#53 Diferencia de Conjuntos.asm
+++ Códigos SASM/#53 Diferencia de Conjuntos.asm
@@ -1,43 +1,65 @@
+; ************************************************************
+; Función: Diferencia de Conjuntos
+; 
+; Descripción:
+;   Calcula la diferencia entre dos conjuntos A y B (A-B),
+;   es decir, encuentra los elementos que están en A pero no en B.
+;
+; Entradas:
+;   - c_a: Cantidad de elementos en el conjunto A (word)
+;   - A: Array con los elementos del conjunto A (word)
+;   - c_b: Cantidad de elementos en el conjunto B (word)
+;   - B: Array con los elementos del conjunto B (word)
+;
+; Salida:
+;   Imprime los elementos que están en A pero no en B
+;
+; Ejemplo:
+;   A = {1,3,4}, B = {1,2,3}
+;   Resultado: 4 (único elemento en A que no está en B)
+; ************************************************************
+
 %include "io.inc"
 
 section .data
-    c_a dw 3
-    A dw 1, 3, 4
-    c_b dw 3
-    B dw 1, 2, 3
+    c_a dw 3          ; Cantidad de elementos en A
+    A dw 1, 3, 4      ; Elementos del conjunto A
+    c_b dw 3          ; Cantidad de elementos en B
+    B dw 1, 2, 3      ; Elementos del conjunto B
        
-    
 section .text
 global CMAIN
 CMAIN:
-    mov ebp, esp; for correct debugging
+    mov ebp, esp      ; Para depuración correcta
     
-    xor eax, eax
-    xor ebx, ebx
+    xor eax, eax      ; Limpia el registro eax
+    xor ebx, ebx      ; Limpia el registro ebx
     
-    mov ax, [c_a]
-    mov bx, [c_b]
+    ; Carga las cantidades de elementos
+    mov ax, [c_a]     ; ax = cantidad de elementos en A
+    mov bx, [c_b]     ; bx = cantidad de elementos en B
     
-    shl eax, 1
-    shl ebx, 1
+    ; Multiplica por 2 (cada elemento ocupa 2 bytes al ser word)
+    shl eax, 1        ; eax *= 2
+    shl ebx, 1        ; ebx *= 2
     
-    xor ecx, ecx
-    xor edx, edx
+    xor ecx, ecx      ; Limpia ecx
+    xor edx, edx      ; Limpia edx
     
-    .loop1:
-        mov cx, [A + eax - 2]
-        .loop2:
-            mov dx, [B + ebx - 2]
-            cmp ecx, edx
-            jz .continue
+    .loop1:           ; Bucle exterior (recorre A)
+        mov cx, [A + eax - 2]    ; Obtiene elemento actual de A
+        .loop2:                  ; Bucle interior (recorre B)
+            mov dx, [B + ebx - 2]; Obtiene elemento actual de B
+            cmp ecx, edx         ; Compara elementos
+            jz .continue         ; Si son iguales, salta
             
-            sub ebx, 2
-            jnz .loop2
+            sub ebx, 2           ; Siguiente elemento de B
+            jnz .loop2          ; Si no es cero, continúa el bucle
         
-        PRINT_DEC 1, ecx
+        PRINT_DEC 1, ecx        ; Imprime el elemento si no se encontró en B
             
         .continue:
-        sub eax, 2
-        jnz .loop1
+        sub eax, 2              ; Siguiente elemento de A
+        jnz .loop1             ; Si no es cero, continúa el bucle
    
     ret
